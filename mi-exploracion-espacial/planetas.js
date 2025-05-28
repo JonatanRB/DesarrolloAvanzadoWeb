d2ad495164003d391213b01c7351e1cd149076ae
Jonatan <jonreabarbosa@gmail.com>
2025-05-27 21:36:13 -0600

    Proyecto 7: Intro Node.js y npm

---
+++ mi-exploracion-espacial/planetas.js
@@ -0,0 +1,29 @@
+const planetas = [
+    {
+        nombre: "Titán",
+        descripcion: "La luna más grande de Saturno, con lagos de metano",
+        descubiertoEn: "1655",
+    },
+    {
+        nombre: "Proxima Centauri b",
+        descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella",
+        descubiertoEn: "2016",
+    },
+    {
+    nombre: "Kepler-22b",
+    descripcion: "Un exoplaneta situado en la zona habitable de su estrella, Kepler-22, donde las condiciones podrían permitir la existencia de agua líquida.",
+    descubiertoEn: "2011"
+},
+{
+    nombre: "Trappist-1e",
+    descripcion: "Uno de los siete planetas del sistema Trappist-1, considerado uno de los más prometedores para la búsqueda de vida fuera del sistema solar.",
+    descubiertoEn: "2017"
+},
+{
+    nombre: "HD 209458 b",
+    descripcion: "Conocido como 'Osiris', fue el primer exoplaneta observado transitando su estrella y el primero en tener una atmósfera detectada.",
+    descubiertoEn: "1999"
+}
+];
+
+module.exports = planetas;
